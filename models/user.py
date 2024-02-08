71600fd1b06925731e3616d0cf5fd9c0c524dfc6
evans-manyala <evans.manyala@gmail.com>
2024-02-08 13:14:15 +0300

    Adding missing code logic

--- models/user.py
+++ models/user.py
@@ -3,3 +3,20 @@
 Defines the User class.
 """
 from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    Represent a User.
+
+    Attributes:
+        first_name (str): User first name.
+        last_name (str): User last name.
+        email (str): User email.
+        password (str): User password.
+    """
+
+    first_name = ""
+    last_name = ""
+    email = ""
+    password = ""
