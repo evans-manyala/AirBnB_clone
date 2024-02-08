3fe0645d09df227a84310d42001015da8543d6f2
evans-manyala <evans.manyala@gmail.com>
2024-02-08 13:10:31 +0300

    Adding missing code logic

--- models/state.py
+++ models/state.py
@@ -3,3 +3,13 @@
 Defines the State class.
 """
 from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
