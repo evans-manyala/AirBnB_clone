e39ff320ff3ddd4bf0a981e3ec95cbadfbca038a
Evans Manyala <evans.manyala@gmail.com>
2024-02-08 09:19:14 +0300

    Adding code structure

--- models/city.py
+++ models/city.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+"""
+Defines the City class.
+"""
 from models.base_model import BaseModel
 
 
+class City(BaseModel):
+    """Represent a city.
+
+    Attributes:
+        state_id (str):  State id.
+        name (str):  Name of the city.
+    """
+
+    state_id = ""
+    name = ""
