e4b27c6e8d611532076cd67d6d4c7cb050510cf1
evans-manyala <evans.manyala@gmail.com>
2024-02-08 15:20:46 +0300

    Adding code logi

--- console.py
+++ console.py
@@ -7,9 +7,28 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
 from models.review import Review
+from models.state import State
+from models.user import User
+
+
+def parser(arg):
+    curly_brackets = re.search(r"\{(.*?)\}", arg)
+    square_brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_brackets is None:
+        if square_brackets is None:
+            return [x.strip(",") for x in split(arg)]
+        else:
+            lexicon = split(arg[:square_brackets.span()[0]])
+            return_line = [x.strip(",") for x in lexicon]
+            return_line.append(square_brackets.group())
+            return return_line
+    else:
+        lexicon = split(arg[:curly_brackets.span()[0]])
+        return_line = [x.strip(",") for x in lexicon]
+        return_line.append(curly_brackets.group())
+        return return_line
+
