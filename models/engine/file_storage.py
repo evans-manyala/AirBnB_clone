95a4fe87ef3a625a94b04e960c2991af0882f077
evans-manyala <evans.manyala@gmail.com>
2024-02-08 13:36:13 +0300

    Changing code logic structure

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,9 +4,9 @@ Define the FileStorage class.
 """
 import json
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
 from models.review import Review
+from models.state import State
+from models.user import User
