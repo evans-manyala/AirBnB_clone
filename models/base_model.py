bd016340cedcc8dbf8e1e28b1f7c3145c8d74d2a
Evans Manyala <evans.manyala@gmail.com>
2024-02-08 08:54:41 +0300

    Adding code structure

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ import models
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
@@ -15,15 +16,39 @@ class BaseModel:
             *args (any): To be used.
             **kwargs (dict): Key or value pairs of attributes.
         """
-        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        timeFormat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, timeformat)
+                    self.__dict__[k] = datetime.strptime(v, timeFormat)
                 else:
                     self.__dict__[k] = v
         else:
             models.storage.new(self)
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Return the dictionary of the BaseModel.
+
+        Including the key or value pair __class__ name.
+        """
+        returnDictionary = self.__dict__.copy()
+        returnDictionary["created_at"] = self.created_at.isoformat()
+        returnDictionary["updated_at"] = self.updated_at.isoformat()
+        returnDictionary["__class__"] = self.__class__.__name__
+        return returnDictionary
+
+    def __str__(self):
+        """
+        Return the string representation of the BaseModel.
+        """
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
