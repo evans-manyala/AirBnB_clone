c9c0dbae2cb24fc63d31861c74882f44a03b1603
evans-manyala <evans.manyala@gmail.com>
2024-02-08 13:19:24 +0300

    Adding missing code logic

--- models/amenity.py
+++ models/amenity.py
@@ -3,3 +3,14 @@
 Defines the Amenity class.
 """
 from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Represent an amenity.
+
+    Attributes:
+        name (str): Name of the amenity found in the place.
+    """
+
+    name = ""
