e91c14db3241e1de08f0a6c0e971db36152a64f2
evans-manyala <evans.manyala@gmail.com>
2024-02-08 13:07:29 +0300

    Adding code structure

--- models/review.py
+++ models/review.py
@@ -5,3 +5,16 @@ Defines the Review class.
 from models.base_model import BaseModel
 
 
+class Review(BaseModel):
+    """
+    Represent a review.
+
+    Attributes:
+        place_id (str): Place id.
+        user_id (str): User id.
+        text (str): Review data input by the user.
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
