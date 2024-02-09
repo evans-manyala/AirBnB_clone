52fea70e6aa86a3e955f549bc158b094c6cc966c
Evans Manyala <evans.manyala@gmail.com>
2024-02-09 21:46:28 +0300

    Initial commit

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 """
 Class defintion for unittests
+
+
+Unittest classes:
+    
+    TestAirBnBCommand_create
+    TestAirBnBCommand_update
+    TestAirBnBCommand_all
+    TestAirBnBCommand_prompt
+    TestAirBnBCommand_destroy
+    TestAirBnBCommand_exit
+    TestAirBnBCommand_show
+    
 """
+import os
+import sys
+import unittest
+from models import storage
+from models.engine import FileStorage
+from io import StringIO
+from unittest.mock import patch
