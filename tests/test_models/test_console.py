f80a26b4c431b392f106a9cb225cdb8e405eceb2
Evans Manyala <evans.manyala@gmail.com>
2024-02-09 21:50:14 +0300

    Adding code logic

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -16,7 +16,20 @@ Unittest classes:
 import os
 import sys
 import unittest
+from console import AirBnBCommand
 from models import storage
 from models.engine import FileStorage
 from io import StringIO
 from unittest.mock import patch
+
+
+class AirBnBCommand_prompt(unittest.TestCase):
+    """Unittests for testing prompting of the HBNB command interpreter."""
+
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", AirBnBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(AirBnBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
