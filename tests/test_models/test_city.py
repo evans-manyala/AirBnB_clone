1c83ac60bc305e664c0660b6687ba452e458b08a
evans-manyala <evans.manyala@gmail.com>
2024-02-13 13:42:00 +0300

    Initial code commit

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines unit tests for models/city.py.
+
+Unit test classes:
+    TestCityInitialization
+    TestCitySave
+    TestCityToDictionary
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.city import City
+
