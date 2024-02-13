534baa2036903d363b6513427a97f48a65d599ea
evans-manyala <evans.manyala@gmail.com>
2024-02-13 18:03:13 +0300

    Adding code logic

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,7 +3,7 @@
 Defines unit tests for models/user.py.
 
 Unittest classes:
-    TestUSerInitialization
+    TestUserInitialization
     TestUserSave
     TestUserToDictionary
 """
@@ -15,7 +15,7 @@ from time import sleep
 from models.user import User
 
 
-class TestUSerInitialization(unittest.TestCase):
+class TestUserInitialization(unittest.TestCase):
     """
     Unit testing Initialization of the User class.
     """
@@ -91,3 +91,112 @@ class TestUSerInitialization(unittest.TestCase):
     def testInitializationWithNoneKwargs(self):
         with self.assertRaises(TypeError):
             User(id=None, created_at=None, updated_at=None)
+
+
+class TestUserSave(unittest.TestCase):
+    """
+    Unit testing SAVE method of the  class.
+    """
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def testOneSave(self):
+        user = User()
+        sleep(0.05)
+        first_updated_at = user.updated_at
+        user.save()
+        self.assertLess(first_updated_at, user.updated_at)
+
+    def testTwoSave(self):
+        user = User()
+        sleep(0.05)
+        first_updated_at = user.updated_at
+        user.save()
+        second_updated_at = user.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        user.save()
+        self.assertLess(second_updated_at, user.updated_at)
+
+    def testSaveWithArg(self):
+        user = User()
+        with self.assertRaises(TypeError):
+            user.save(None)
+
+    def testSaveUpdatesFile(self):
+        user = User()
+        user.save()
+        user_id = "User." + user.id
+        with open("file.json", "r") as f:
+            self.assertIn(user_id, f.read())
+
+
+class TestUserToDictionary(unittest.TestCase):
+    """
+    Unit testing ToDictionary method of the User class.
+    """
+
+    def testToDictionaryType(self):
+        self.assertTrue(dict, type(User().to_dict()))
+
+    def testToDictionaryContainsCorrectKeys(self):
+        user = User()
+        self.assertIn("id", user.to_dict())
+        self.assertIn("created_at", user.to_dict())
+        self.assertIn("updated_at", user.to_dict())
+        self.assertIn("__class__", user.to_dict())
+
+    def testToDictionaryContainsAddedAttr(self):
+        user = User()
+        user.middle_name = "Holberton"
+        user.my_number = 98
+        self.assertEqual("Holberton", user.middle_name)
+        self.assertIn("my_number", user.to_dict())
+
+    def testToDictionaryDateTimeAttrAreStrings(self):
+        user = User()
+        user_dict = user.to_dict()
+        self.assertEqual(str, type(user_dict["id"]))
+        self.assertEqual(str, type(user_dict["created_at"]))
+        self.assertEqual(str, type(user_dict["updated_at"]))
+
+    def testToDictionaryOutput(self):
+        dateTime = datetime.today()
+        user = User()
+        user.id = "123456"
+        user.created_at = user.updated_at = dateTime
+        tdict = {
+            'id': '123456',
+            '__class__': 'User',
+            'created_at': dateTime.isoformat(),
+            'updated_at': dateTime.isoformat(),
+        }
+        self.assertDictEqual(user.to_dict(), tdict)
+
+    def testContrastToDictionaryDunderDictionary(self):
+        user = User()
+        self.assertNotEqual(user.to_dict(), user.__dict__)
+
+    def testToDictionaryWithArg(self):
+        user = User()
+        with self.assertRaises(TypeError):
+            user.to_dict(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
