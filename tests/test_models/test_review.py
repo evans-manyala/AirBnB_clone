18d3b267c18707e8aa20179b779f49e825ba4dab
evans-manyala <evans.manyala@gmail.com>
2024-02-13 16:52:12 +0300

    Adding code logic

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -97,3 +97,112 @@ class TestReview_instantiation(unittest.TestCase):
     def testInitializationNoneKwargs(self):
         with self.assertRaises(TypeError):
             Review(id=None, created_at=None, updated_at=None)
+
+
+class TestPlaceSave(unittest.TestCase):
+    """
+    Unit testing SAVE method of the Review class.
+    """
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def testOneSave(self):
+        re_view = Review()
+        sleep(0.05)
+        first_updated_at = re_view.updated_at
+        re_view.save()
+        self.assertLess(first_updated_at, re_view.updated_at)
+
+    def testTwoSaves(self):
+        re_view = Review()
+        sleep(0.05)
+        first_updated_at = re_view.updated_at
+        re_view.save()
+        second_updated_at = re_view.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        re_view.save()
+        self.assertLess(second_updated_at, re_view.updated_at)
+
+    def testSaveWithArg(self):
+        re_view = Review()
+        with self.assertRaises(TypeError):
+            re_view.save(None)
+
+    def testSaveUpdatesFile(self):
+        re_view = Review()
+        re_view.save()
+        re_viewid = "Review." + re_view.id
+        with open("file.json", "r") as f:
+            self.assertIn(re_viewid, f.read())
+
+
+class TestPlaceToDictionary(unittest.TestCase):
+    """
+    Unit testing ToDictionary method of the Review class.
+    """
+
+    def testToDictionaryType(self):
+        self.assertTrue(dict, type(Review().to_dict()))
+
+    def testToDictionaryContainsCorrectKeys(self):
+        re_view = Review()
+        self.assertIn("id", re_view.to_dict())
+        self.assertIn("created_at", re_view.to_dict())
+        self.assertIn("updated_at", re_view.to_dict())
+        self.assertIn("__class__", re_view.to_dict())
+
+    def testToDictionaryContainsAddedAttr(self):
+        re_view = Review()
+        re_view.middle_name = "Holberton"
+        re_view.my_number = 98
+        self.assertEqual("Holberton", re_view.middle_name)
+        self.assertIn("my_number", re_view.to_dict())
+
+    def testToDictionaryDateTimeAttrAreStrings(self):
+        re_view = Review()
+        re_view_dict = re_view.to_dict()
+        self.assertEqual(str, type(re_view_dict["id"]))
+        self.assertEqual(str, type(re_view_dict["created_at"]))
+        self.assertEqual(str, type(re_view_dict["updated_at"]))
+
+    def testToDictionaryOutput(self):
+        dateTime = datetime.today()
+        re_view = Review()
+        re_view.id = "123456"
+        re_view.created_at = re_view.updated_at = dateTime
+        tdict = {
+            'id': '123456',
+            '__class__': 'Review',
+            'created_at': dateTime.isoformat(),
+            'updated_at': dateTime.isoformat(),
+        }
+        self.assertDictEqual(re_view.to_dict(), tdict)
+
+    def testContrastToDictionaryDunderDictionary(self):
+        re_view = Review()
+        self.assertNotEqual(re_view.to_dict(), re_view.__dict__)
+
+    def testPlaceToDictionaryWithArg(self):
+        re_view = Review()
+        with self.assertRaises(TypeError):
+            re_view.to_dict(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
